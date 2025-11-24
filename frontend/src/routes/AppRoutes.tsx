448f23b6f9729219bcc7e2b9b45bbe93792cb50b
Nadeem-pc <nadeempc777@gmail.com>
2025-11-24 11:19:42 +0530

    feat(routes): add HomePage route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,8 +1,12 @@
 import AuthPage from "@/pages/auth/Auth";
+import HomePage from "@/pages/user/Home";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
     {
         path: '/auth', element: <AuthPage />
+    },
+    {
+        path: '/home', element: <HomePage />
     },
 ]);
