41e32f52caae0f949e250a3dff5d5ac28d1bf2a3
Nadeem-pc <nadeempc777@gmail.com>
2025-12-17 20:14:06 +0530

    refactor(auth-di): simplify auth controller dependency injection

--- backend/src/dependencies/auth.di.ts
+++ backend/src/dependencies/auth.di.ts
@@ -1,18 +1,5 @@
-//dependencies/auth.di.ts
 import { AuthController } from "../controllers/implements/auth.controller"
 import { UserRepository } from "../repositories/implements/user.repository";
 import { AuthService } from "../services/implements/auth.service";
 
-export const authController = new AuthController(
-  new AuthService(new UserRepository())
-);
-
-
-// // 1) Low-level dependency
-// const userRepository = new UserRepository();
-
-// // 2) Service depends on repository
-// const authService = new AuthService(userRepository);
-
-// // 3) Controller depends on service
-// export const authController = new AuthController(authService);
+export const authController = new AuthController(new AuthService(new UserRepository()));
