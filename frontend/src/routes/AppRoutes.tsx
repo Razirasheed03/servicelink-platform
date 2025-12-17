67984e42fed65bbb113b06d108997fe430c68337
Nadeem-pc <nadeempc777@gmail.com>
2025-12-17 21:14:45 +0530

    feat(routes): add provider profile and detail routes with role protection

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,4 +1,3 @@
-// routes/AppRoutes.tsx
 import AuthPage from "@/pages/auth/Auth";
 import HomePage from "@/pages/user/Home";
 import VerifyOtpPage from "@/pages/auth/VerifyOtp";
@@ -6,51 +5,42 @@ import { createBrowserRouter } from "react-router-dom";
 import ProviderHome from "@/pages/provider/HomePage";
 import PublicRoute from "./PublicRoute";
 import ProtectedRoute from "./ProtectedRoute";
+import ServiceProviderProfilePage from "@/pages/provider/Profile";
+import ServiceProviderDetailPage from "@/pages/user/DetailPage";
 
 export const router = createBrowserRouter([
-  // PUBLIC AUTH PAGE
   {
     path: "/auth",
-    element: (
-      <PublicRoute>
-        <AuthPage />
-      </PublicRoute>
-    ),
+    element: <PublicRoute> <AuthPage /> </PublicRoute>
   },
 
-  // PUBLIC OTP PAGE
   {
     path: "/auth/verify-otp",
     element: <VerifyOtpPage />,
   },
 
-  // USER HOME (only user allowed)
-  {
-    path: "/user/home",
-    element: (
-      <ProtectedRoute allowedRoles={["user"]}>
-        <HomePage />
-      </ProtectedRoute>
-    ),
-  },
-
-  // PROVIDER HOME (only service provider allowed)
-  {
-    path: "/provider/home",
-    element: (
-      <ProtectedRoute allowedRoles={["service_provider"]}>
-        <ProviderHome />
-      </ProtectedRoute>
-    ),
-  },
-
-  // DEFAULT ROUTE → user home (protected)
   {
     path: "/",
-    element: (
-      <ProtectedRoute allowedRoles={["user"]}>
-        <HomePage />
-      </ProtectedRoute>
-    ),
+    element: <ProtectedRoute allowedRoles={["user"]}> <HomePage /> </ProtectedRoute>
+  },
+
+  {
+    path: "/user/home",
+    element: <ProtectedRoute allowedRoles={["user"]}> <HomePage /> </ProtectedRoute>
+  },
+
+  {
+    path: "/detail/:id",
+    element: <ProtectedRoute allowedRoles={["user"]}> <ServiceProviderDetailPage /> </ProtectedRoute>
+  },
+
+  {
+    path: "/provider/home",
+    element: <ProtectedRoute allowedRoles={["service_provider"]}> <ProviderHome /> </ProtectedRoute>
+  },
+
+  {
+    path: "/provider/profile",
+    element: <ProtectedRoute allowedRoles={["service_provider"]}> <ServiceProviderProfilePage /> </ProtectedRoute>
   },
 ]);
